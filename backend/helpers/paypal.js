5414b5e43dddd20ecd48df60321fefe46249d6f8
lasandi2003 <anujinisamarasinghe10@gmail.com>
2025-09-23 16:05:53 +0530

    lasandi-added paypal

--- backend/helpers/paypal.js
+++ backend/helpers/paypal.js
@@ -2,8 +2,8 @@ const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
   mode: "sandbox",
-  client_id: "dummy",
-  client_secret: "dummy",
+  client_id: "AVtWzrPL5z0_nEDa8u7EEdQJAR0aD9eNZTfhNpv5KTbgTycji16YGaCUqketMYnYFz9bOoE9xFYSQnwf",
+  client_secret: "EDTXZLD24iTzAu04Wk9YxP9dBaORKNpW8BfV1Sdj8F_UXyHHiTMC_HaIY-JX7DLP9qUmayiGXLs-Ddkf",
 });
 
 module.exports = paypal;
